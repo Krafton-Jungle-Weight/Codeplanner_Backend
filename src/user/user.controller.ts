83c7e7a34b92189f2442850c7f74d0a8e061379e
MyeongSeok11 <myong2404@gmail.com>
2025-06-30 20:34:06 +0900

    CP-27 마이페이지 닉네임변경 구현 이메일 재인증

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -13,16 +13,19 @@ import { ResendEmailDto } from './dto/resend-email.dto';
 import { VerifyEmailDto } from 'src/email/dto/verify-email.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { CurrentUser } from 'src/auth/user.decorator';
+import { UpdateUserDisplayNameDto } from './dto/update-user-displayname.dto';
 
 @Controller('User')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  // 회원가입
   @Post('/create')
   async createUser(@Body() input: CreateUserDto): Promise<void> {
     await this.userService.createUser(input);
   }
 
+  // 이메일 인증
   @Get('/email-verify')
   async verifyEmail(
     @Query('email') email: string,
@@ -31,11 +34,15 @@ export class UserController {
     return await this.userService.verifyEmail(email, verifyToken);
   }
 
+  // 이메일 재전송
+  @UseGuards(JwtAuthGuard)
   @Post('/email-resend')
-  async resendEmail(@Body() input: ResendEmailDto) {
-    return await this.userService.resendJoinEmail(input.email);
+  async resendEmail(@CurrentUser() user: any) {
+    this.userService.resendJoinEmail(user.email);
+    return { message: '이메일 재전송 완료' };
   }
 
+  // 마이페이지
   @Get('/mypage')
   @UseGuards(JwtAuthGuard)
   async myPage(@CurrentUser() user: any) {
@@ -44,8 +51,23 @@ export class UserController {
     return await this.userService.myPage(user.id);
   }
 
-  @Get('/mypage/update')
-  async updageMyPage(@CurrentUser() user: any) {
-    return await this.userService.updateMyPage(user.id);
+  // 닉네임 변경
+  @Post('/mypage/updateDisplayName')
+  @UseGuards(JwtAuthGuard)
+  async updateDisplayName(
+    @CurrentUser() user: any,
+    @Body() input: UpdateUserDisplayNameDto,
+  ) {
+    return await this.userService.updateDisplayName(
+      user.id,
+      input.display_name,
+    );
+  }
+
+  // 이메일 인증 여부 확인
+  @Get('/mypage/isVerified')
+  @UseGuards(JwtAuthGuard)
+  async isVerified(@CurrentUser() user: any) {
+    return await this.userService.isVerified(user.id);
   }
 }
