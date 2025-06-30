f16495d6e7f91a4e33b39f6db19a93694227e345
prkty <prkty0@gmail.com>
2025-06-30 21:57:34 +0900

    개발환경시 localhost:5000, 배포환경시 EC2IP:5000 설정하게끔 main.ts, app.module.ts, 패키지 추가 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,7 @@
+// ⚠️⚠️⚠️배포환경 관련 설정 주의 필요!!!⚠️⚠️⚠️
+
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './modules/app/app.controller';
 import { AppService } from './modules/app/app.service';
 import { UserModule } from './user/user.module';
@@ -10,7 +13,12 @@ import { IssuesModule } from './issues/issues.module';
 import { TimelineModule } from './timeline/timeline.module';
 
 @Module({
-  imports: [UserModule, AuthModule, EmailModule, DatabaseModule, IssuesModule, TimelineModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development',  // 개발 환경시: .env.development 파일 사용, 배포 환경시: .env.production 파일 사용
+    }),
+    UserModule, AuthModule, EmailModule, DatabaseModule, IssuesModule, TimelineModule],
   controllers: [AppController, EmailController],
 
   providers: [AppService],
