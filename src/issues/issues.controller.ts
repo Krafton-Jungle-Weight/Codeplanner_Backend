ee7a871301a93ff7f5289b9aed560cf6bfd0f27c
김태용 <prkty0@gmail.com>
2025-07-02 05:58:03 +0900

    Revert "07/02 CP-12 git feat(issue): 이슈 카운트 컨트롤러, 서비스 구현"

--- src/issues/issues.controller.ts
+++ src/issues/issues.controller.ts
@@ -1,13 +1,4 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { IssuesService } from './issues.service';
 import { CreateIssueDto, ReorderIssuesDto } from './issues-update.dto';
 import { UpdateIssueDto } from './dto/issue-info.dto';
@@ -15,9 +6,7 @@ import { Issue } from './issues.entity';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { CurrentUser } from 'src/auth/user.decorator';
 
-
-
-Controller('projects')
+@Controller('projects')
 export class IssuesController {
   constructor(private readonly issuesService: IssuesService) {}
 
@@ -52,23 +41,10 @@ export class IssuesController {
   }
 
   @UseGuards(JwtAuthGuard)
-  @Get('/projects/:projectId/my-issues-count')
-  async getMyIssueCount(
-    @CurrentUser() user: any,
-    @Param('projectId') projectId: string,
-  ): Promise<{ count: number }> {
-    const count = await this.issuesService.getIssuesCurrentUserCount(
-      user.id,
-      projectId,
-    );
-    return { count };
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('/projects/:projectId/my-issues')
+  @Get('/:projectId/my-issues')
   getMyIssue(
-    @CurrentUser() user: any,
-    @Param('projectId') projectId: string,
+  @CurrentUser() user: any,
+  @Param('projectId') projectId: string,
   ): Promise<Issue[]> {
     return this.issuesService.getIssuesCurrentUser(user.id, projectId);
   }
@@ -94,22 +70,22 @@ export class IssuesController {
   deleteIssue(
     @Param('issueId') issueId: string,
     @Param('projectId') projectId: string,
-  ) { 
+  ) {
     return this.issuesService.deleteIssue(issueId, projectId);
   }
 
   @Post('/issues/:id/update-dates')
   async updateIssueDates(
     @Param('id') id: string,
-    @Body() body: { startDate: string; dueDate: string; projectId: string },
+    @Body() body: { startDate: string, dueDate: string, projectId: string }
   ) {
     return this.issuesService.updateIssueInfo(
-      {
-        startDate: body.startDate ? new Date(body.startDate) : undefined,
-        dueDate: body.dueDate ? new Date(body.dueDate) : undefined,
+      { 
+        startDate: body.startDate ? new Date(body.startDate) : undefined, 
+        dueDate: body.dueDate ? new Date(body.dueDate) : undefined 
       },
       body.projectId,
-      id,
+      id
     );
   }
 }
