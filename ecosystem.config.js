0d3b71e22a223a56e3972e9b651bcb1b929cb30f
prkty <prkty0@gmail.com>
2025-07-02 00:25:25 +0900

    404 조사중

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   apps: [
     {
-      name: 'Codeplanner-Backend',
+      name: 'Codeplanner_Backend',
       script: 'dist/main.js',
       instances: 1,
       autorestart: true,
@@ -15,7 +15,6 @@ module.exports = {
         NODE_ENV: 'production',
         PORT: 5000,
         CORS_ORIGIN: 'http://3.38.25.129,http://3.38.25.129:3000,http://3.38.25.129:5000',
-        BASE_URL: 'http://3.38.25.129:5000'
       },
     },
   ],
