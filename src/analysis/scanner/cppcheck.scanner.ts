313939cd1725f7861edc2a12a83dce0c09c940eb
kimyounhoex1 <dordyd11@gmail.com>
2025-07-04 05:35:15 +0900

    07/04 CP-33 #71 feat(codeclean): 초기 실행되는 명령어 최종 확인, 로그 정상 작동

--- src/analysis/scanner/cppcheck.scanner.ts
+++ src/analysis/scanner/cppcheck.scanner.ts
@@ -2,11 +2,11 @@ import { execa } from "execa";
 import { join } from "path";
 
 export async function runCppcheck(): Promise<string> {
-  const filePath = join(__dirname, '..', '..', 'static', 'code-samples', 'example1.c');
+  const filePath = join(__dirname, '..', '..', '..', 'src', 'static', 'code-samples', 'example1.c');
   console.log('Cppcheck filePath:', filePath);
 
   try {
-    const { stdout, stderr } = await execa('cppcheck', [filePath]);
+    const { stdout, stderr } = await execa('cppcheck', ['--enable=all', filePath]);
     return stdout + '\n' + stderr;
   } catch (error: any) {
     return error.stdout || error.stderr || error.message;
