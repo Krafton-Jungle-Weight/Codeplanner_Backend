8c72feec0c815e9acf7b5cdc409ae31b316f5f46
김태용 <prkty0@gmail.com>
2025-07-08 22:11:30 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -16,13 +16,13 @@ async function bootstrap() {
   // 모든 라우트에 /api 프리픽스 적용
   // app.setGlobalPrefix('api');
 
-  // Nginx에서 CORS를 처리하므로 백엔드에서는 비활성화
-  // const corsOrigin = process.env.CORS_ORIGIN?.split(',') || ['http://localhost:3000'];
-  // app.enableCors({
-  //   origin: corsOrigin,
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   credentials: true,
-  // });
+  // .env에서 CORS_ORIGIN을 불러와서 배열로 변환
+  const corsOrigin = process.env.CORS_ORIGIN?.split(',') || ['http://localhost:3000'];  // 개발 환경시: http://localhost:3000, 배포 환경시: 배포 주소
+  app.enableCors({
+    origin: corsOrigin,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   const port = process.env.PORT || 5000;  // 백엔드 포트를 5000으로 설정
   await app.listen(port);
