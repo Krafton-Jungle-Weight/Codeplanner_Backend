60da7595932cc59ef18d5b1e0f025245634024f9
jkh1447 <jkh1447@gmail.com>
2025-07-13 00:37:37 +0900

    25-07-13 이슈 추가 모달 및 칸반보드 한글화, 알림 드롭다운 메뉴에서 삭제기능, 알림 모두보기 페이지, 백로그 및 자신에게 할당된 이슈에 대한 알림 구현

--- src/notification/notification.entity.ts
+++ src/notification/notification.entity.ts
@@ -10,21 +10,12 @@ export class Notification {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ type: 'uuid', name: 'user_id' })
-  userId: string;
-
   @Column({ type: 'varchar', length: 50 })
   type: string; // e.g. 'message', 'system', 'comment'
 
   @Column({ type: 'jsonb' })
   payload: any; // 알림 내용을 담는 구조체
 
-  @Column({ type: 'boolean', default: false, name: 'is_read' })
-  isRead: boolean;
-
   @CreateDateColumn({ type: 'timestamptz', name: 'created_at' })
   createdAt: Date;
-
-  @Column({ type: 'timestamptz', nullable: true, name: 'read_at' })
-  readAt: Date | null;
 }
